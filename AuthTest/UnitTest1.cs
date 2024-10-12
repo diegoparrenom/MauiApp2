4dcc389743160bdfe893a8e1aef56186fdf7c35f
dparrenom <diegoparrenom@gmail.com>
2024-10-12 18:07:41 -0500

    Se agregan pruebas unitarias y se modifican colores para modo dark

--- AuthTest/UnitTest1.cs
+++ AuthTest/UnitTest1.cs
@@ -30,9 +30,72 @@ namespace AuthTest
         public async Task TestGenresNumber()
         {
             resultToken = await LoginTest.GetLogin();
-          
+
             var genres_list = await AccountTest.GetGenres(resultToken);
             Assert.InRange(genres_list.Count, 5, 200);
         }
+        [Fact]
+        public async Task TestIsFeaturedPlaylist()
+        {
+            resultToken = await LoginTest.GetLogin();
+
+            var FeatPlaylist = await AccountTest.GetFeaturedPlaylist(resultToken);
+            Assert.IsType<MauiApp2.Model.PlaylistGroup>(FeatPlaylist);
+
+        }
+        [Fact]
+        public async Task TestGetFeaturedPlaylistNumber()
+        {
+            resultToken = await LoginTest.GetLogin();
+
+            var FeatPlaylist = await AccountTest.GetFeaturedPlaylist(resultToken);
+            Assert.InRange(FeatPlaylist.playlists.items.Length, 5, 200);
+
+        }
+        [Fact]
+        public async Task TestIsSinglePlaylist()
+        {
+            resultToken = await LoginTest.GetLogin();
+
+            var SinglePlaylist = await AccountTest.GetSinglePlaylist(resultToken, "3cEYpjA9oz9GiPac4AsH4n");
+            Assert.IsType<MauiApp2.Model.Playlist>(SinglePlaylist);
+        }
+        [Fact]
+        public async Task TestGetSinglePlaylistNumber()
+        {
+            resultToken = await LoginTest.GetLogin();
+
+            var SinglePlaylist = await AccountTest.GetSinglePlaylist(resultToken, "3cEYpjA9oz9GiPac4AsH4n");
+            Assert.InRange(SinglePlaylist.tracks.items.Count, 5, 200);
+        }
+        [Fact]
+        public async Task TestIsPlayList()
+        {
+            resultToken = await LoginTest.GetLogin();
+
+            var FeatPlaylist = await AccountTest.GetFeaturedPlaylist(resultToken);
+
+            var Playlist_id = FeatPlaylist.playlists.items.First().id;
+
+            var result_playlist = await AccountTest.getPlayList(resultToken, Playlist_id);
+
+            Assert.IsType<MauiApp2.Model.Playlist>(result_playlist);
+
+        }
+        [Fact]
+        public async Task TestGetPlayListNumber()
+        {
+            resultToken = await LoginTest.GetLogin();
+
+            var FeatPlaylist = await AccountTest.GetFeaturedPlaylist(resultToken);
+
+            var Playlist_id = FeatPlaylist.playlists.items.First().id;
+
+            var result_playlist = await AccountTest.getPlayList(resultToken, Playlist_id);
+
+            Assert.InRange(result_playlist.tracks.items.Count, 5, 200);
+
+        }
+
     }
 }
